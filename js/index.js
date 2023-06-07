3d8c9732a98ca1bec761140273aa5bce98bf168c
EricaDianaa <ericadiana2003@gmail.com>
2023-06-07 18:03:41 +0200

    c

--- js/index.js
+++ js/index.js
@@ -14,12 +14,21 @@ documentActivity.innerHTML = `
     `
     const saveActivity= document.getElementById("saveactivity")
 saveActivity.appendChild(documentActivity)
-const deleteButton=document.querySelectorAll("saveactivity,button")
+const deleteButton=document.querySelectorAll("#saveactivity")
 deleteButton.forEach(button=>{
   button.addEventListener("click",function(e){
     let buttonclick= e.target
     let clickMe=buttonclick.parentElement
     clickMe.remove()
+
+    const deleteP=document.querySelectorAll("#saveactivity")
+    deleteP.forEach(p=>{
+p.addEventListener("click",function(p){
+  let addline=p.target
+  let click1=addline.parentElement
+  click1.style.textDecoration="line-through;"
+})
+    })
   })
 })
 activity.value=""
